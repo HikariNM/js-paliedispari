51f158d1965afd30949f98cb868e4acf22524b3c
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-06 18:29:44 +0100

    creata seconda versione

--- Palidroma/Palidroma.js
+++ Palidroma/Palidroma.js
@@ -1,19 +1,37 @@
-function checkPalindrome(userWord){
-    const wordArray = userWord.split('');
-    const reverseWordArray = wordArray.reverse();
-    const backToString = reverseWordArray.join('');
+// function checkPalindrome(string){
+//     const wordArray = userWord.split('');
+//     const reverseWordArray = wordArray.reverse();
+//     const backToString = reverseWordArray.join('');
 
-    if( userWord === backToString){
-        console.log('Affascinante… la parola si specchia e resta identica. Che equilibrio perfetto')
-        alert('Affascinante… la parola si specchia e resta identica. Che equilibrio perfetto')
-    }
-    else {
-        console.log('Ah… peccato. Questa parola non ha il coraggio di guardarsi allo specchio')
-        alert('Ah… peccato. Questa parola non ha il coraggio di guardarsi allo specchio')
-    }
+//     if( userWord === backToString){
+//         console.log('Affascinante… la parola si specchia e resta identica. Che equilibrio perfetto')
+//         alert('Affascinante… la parola si specchia e resta identica. Che equilibrio perfetto')
+//     }
+//     else {
+//         console.log('Ah… peccato. Questa parola non ha il coraggio di guardarsi allo specchio')
+//         alert('Ah… peccato. Questa parola non ha il coraggio di guardarsi allo specchio')
+//     }
+// }
+
+// const userWord = prompt('Inserisci una parola… vediamo se saprà riflettersi da sola')
+// console.log(`${userWord}... scelta audace`)
+// checkPalindrome(userWord);
+
+
+
+function checkPalindromeWithLoop(string){
+    const len = string.length;
+    
+    for (let i = 0; i < len / 2; i++) {
+        
+        if (string[i] !== string[len - 1 - i]) {
+            return 'Ah… peccato. Questa parola non ha il coraggio di guardarsi allo specchio'
+        }
+        
+    } return 'Affascinante… la parola si specchia e resta identica. Che equilibrio perfetto'
 }
 
 const userWord = prompt('Inserisci una parola… vediamo se saprà riflettersi da sola')
 console.log(`${userWord}... scelta audace`)
 
-checkPalindrome(userWord);
+alert(checkPalindromeWithLoop(userWord));
